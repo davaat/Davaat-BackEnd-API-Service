62f914e045719bc2dbc78e92686b4f41833fbef2
Fan Wang <nimadorostkar97@gmail.com>
2023-03-07 10:55:42 +0330

    UserSubscription

--- app/subscription/models.py
+++ app/subscription/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.db.models import Model
+from authentication.models import User
 
-# Create your models here.
+
+
+class Subscription(Model):
+    title = models.CharField(max_length=256, unique=True)
+    price = models.CharField(max_length=256)
+    signature_number = models.IntegerField()
+    contract_number = models.IntegerField()
+    duration_day = models.IntegerField()
+
+    def __str__(self):
+        return str(self.title)
+
+
+class UserSubscription(Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+    subscription = models.ForeignKey(Subscription, on_delete=models.CASCADE, related_name='Subscription')
+    start_date = models.CharField(max_length=256)
+    end_date = models.CharField(max_length=256)
+    CHOICES = (('فعال', 'فعال'),('به اتمام رسیده', 'به اتمام رسیده'),('لغو شده', 'لغو شده'))
+    status = models.CharField(max_length=256, choices=CHOICES, null=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.subscription.title) +'|'+ str(self.user)
