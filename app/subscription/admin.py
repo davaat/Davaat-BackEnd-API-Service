1ca4a310736c3d185aa522b18b38d4debf7a3194
Fan Wang <nimadorostkar97@gmail.com>
2023-03-07 10:59:35 +0330

    UserSubscriptionAdmin

--- app/subscription/admin.py
+++ app/subscription/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Subscription, UserSubscription
 
-# Register your models here.
+
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'status', 'start_date')
+admin.site.register(Subscription, SubscriptionAdmin)
+
+
+class UserSubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'signature_number', 'contract_number', 'duration_day')
+admin.site.register(UserSubscription, UserSubscriptionAdmin)
