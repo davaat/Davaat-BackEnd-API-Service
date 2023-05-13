73a9d0e85cf5a8840636ab7df907158f285d87a6
Mahdi1419 <99290119+Mahdi1419@users.noreply.github.com>
2023-05-14 01:03:32 +0330

    Add 'corsheaders' to install apps and config that
    Added 'django-cors-headers' middleware to core middlewars
    and  'CORS_ALLOW_ALL_ORIGINS' set to 'True' for allow all origins

--- app/core/settings.py
+++ app/core/settings.py
@@ -50,13 +50,14 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+CORS_ALLOW_ALL_ORIGINS = True # All origins will be allowed
 ROOT_URLCONF = 'core.urls'
 TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")  # ROOT dir for templates
 
