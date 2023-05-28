a39716d0eb9ec89107539a635041bfbad3d7960b
Fan Wang <nimadorostkar97@gmail.com>
2023-05-28 10:56:54 +0330

    update

--- app/reminders/models.py
+++ app/reminders/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from authentication.models import User
 
-
 class Reminder(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reminder_user')
     title = models.CharField(max_length=256)
